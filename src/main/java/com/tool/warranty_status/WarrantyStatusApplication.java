3c17555a7dc5d5454bb9aac86b210028d8992090
ismailbozan90 <ismailbozan90@gmail.com>
2024-07-10 14:07:06 +0300

    first commit

---
+++ src/main/java/com/tool/warranty_status/WarrantyStatusApplication.java
@@ -0,0 +1,13 @@
+package com.tool.warranty_status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarrantyStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarrantyStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tool/warranty_status/WarrantyStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tool.warranty_status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarrantyStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
